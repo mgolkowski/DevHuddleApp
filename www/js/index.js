3b11a80573ed37ff6c7df779b76c87930ae04aea
mgolkowski <Michael.Golkowski@worldvision.com.au>
2016-08-01 16:25:12 +1000

    test

--- www/js/index.js
+++ www/js/index.js
@@ -70,7 +70,7 @@ var app = {
                             app.getServerTOCUpdate("1900-01-01");
 
                         }, function (e) {
-                            alert("ERROR: " + e.message);
+                            alert("ERROR in Insert: " + e.message);
                         });
 
                     } else { // already has a row - read it
@@ -82,7 +82,7 @@ var app = {
                                 app.doServerTOCUpdate(retval);
 
                             }, function (e) {
-                                alert("ERROR: " + e.message);
+                                alert("ERROR in Select: " + e.message);
                             });
                         });
                     }
@@ -209,6 +209,12 @@ var app = {
 
                     alert('reading article.');
                     alert('rows: ' + res.rows.item.length);
+
+                    if (res.rows.item.length == 0) { // article not loaded in db yet - load and display
+
+                    } else { // article exists - compare last update timestamp with TOC 
+
+                    }
 
                 }, function (e) {
                     alert("ERROR: " + e.message);
