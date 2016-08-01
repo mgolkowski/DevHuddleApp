af53742fd53c4740216cfa64b16815d9cdd1e16f
mgolkowski <Michael.Golkowski@worldvision.com.au>
2016-08-01 12:33:16 +1000

    test

--- www/js/index.js
+++ www/js/index.js
@@ -54,35 +54,40 @@ var app = {
 
         db.transaction(function (tx) {
 
-
+            // Create DB table (if not already created) to store last TOC update
             tx.executeSql('CREATE TABLE IF NOT EXISTS LastTOCUpdate (lastUpdate text)');
             db.transaction(function (tx) {
 
+                // DB exists or has been created.  Check for a row to hold data
                 tx.executeSql("SELECT COUNT(*) AS cnt from LastTOCUpdate;", [], function (tx, res) {
                     var numRows = res.rows.item(0).cnt;
 
                     alert('num rows: ' + numRows);
 
-                    if (numRows == 0) { // first time - set last update to 1900 to force initial refresh
+                    if (numRows == 0) { // no row =first time - set last update to 1900 to force initial refresh
                         alert('COUNT = 0 - about to insert');
                         tx.executeSql("INSERT INTO LastTOCUpdate (lastUpdate) VALUES (?)", ["1900-01-01"], function (tx, res) {
-                            alert("rowsAffected: " + res.rowsAffected + " -- should be 1");
+                            
+
+                            app.getServerTOCUpdate("1900-01-01");
 
                         }, function (e) {
                             alert("ERROR: " + e.message);
                         });
 
-                    } else {
+                    } else { // already has a row - read it
                         alert('already has a row - read it ...');
 
                         db.transaction(function (tx) {
+                            alert('reading ...');
                             tx.executeSql("select top 1 lastUpdateText as lastUpdate from LastTOCUpdate;", [], function (tx, res) {
+                                alert('done reading.');
+
                                 var retval = res.rows.item(0).lastUpdate;
                                 alert('result: ' + retval);
 
-                                app.getServerTOCUpdate();
+                                app.getServerTOCUpdate(retval);
 
-                                return retval;
                             });
                         });
                     }
@@ -91,7 +96,8 @@ var app = {
         });
     },
 
-    getServerTOCUpdate: function () {
+    // checks TOC timestamp on server, and if > lastTimestamp then refresh TOC in database
+    doServerTOCUpdate: function (lastTimestamp) {
         $.ajax({
             url: baseURL + 'LastTOCUpdate.xml',
             dataType: "xml",
