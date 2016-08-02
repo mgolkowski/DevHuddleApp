88e6531a77dac926fa1fbc80e32177188a3b90f4
mgolkowski <Michael.Golkowski@worldvision.com.au>
2016-08-02 13:15:22 +1000

    test

--- www/js/index.js
+++ www/js/index.js
@@ -227,7 +227,7 @@ var app = {
                 var numRows = res.rows.length;
                 var html = '';
                 for (var i = 0; i < numRows; i++) {
-                    html += '<div><a href="#" onclick="app.viewArticle(' + res.rows.item(i) + '); return false">' + res.rows.item(i).title + '</a><p>' + res.rows.item(i).dscr + '</p></div>';
+                    html += '<div><a href="#" onclick="alert(\'1\'); app.viewArticle(' + res.rows.item(i) + '); alert(\'2\'); return false">' + res.rows.item(i).title + '</a><p>' + res.rows.item(i).dscr + '</p></div>';
                 }
                 $('#divTOC').html(html).show();
                 $('#deviceready').hide();
