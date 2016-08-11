81db6d58b4f9de7a7feb2914f9608797196792f7
mgolkowski <Michael.Golkowski@worldvision.com.au>
2016-08-11 16:17:26 +1000

    test

--- www/js/index.js
+++ www/js/index.js
@@ -36,19 +36,13 @@ var app = {
 
     // set TOC.isDownloaded = true for all articles in Article table
     populateTOCisDownloaded: function () {
-        alert('in populateTOCisDownloaded');
 
         db.transaction(function (tx) {
-            alert('about to run update');
             tx.executeSql("UPDATE TOC SET isDownloaded = 0", [], function (tx, res) {
-                alert('update done');
 
                 tx.executeSql("SELECT * FROM Article;", [], function (tx, res) {
-                    alert('select done.');
                     for (var i = 0; i < res.rows.item.length; i++) {
-                        alert('i = ' + i);
-                        alert('id = ' + res.rows.item(i).id);
-                        alert('processing id ' + res.rows.item(i).id);
+                        tx.executeSql("UPDATE TOC SET isDownloaded = 1 WHERE id = ?", [res.rows.item(i).id]);
                     }
                 });
             });
